05f8a02b90d4c69b9f1013738556f153f1043170
Lucero Amaolo <michilnx@gmail.com>
2021-02-07 14:54:07 -0300

    Bugfixes en los estilos de styled-components

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap');
-  
+export const GlobalStyle = createGlobalStyle`  
   html {
     font-size: 62.5%;
   }
